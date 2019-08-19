c920cc6ed0b8b4020f655ebcf330a246cb8ca9b6
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-08-20 00:40:31 +0100

    route animation

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -9,6 +10,7 @@ import { ProductData } from './products/product-data';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+
 
 /* Feature Modules */
 import { ProductModule } from './products/product.module';
@@ -20,6 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
     ProductModule,
