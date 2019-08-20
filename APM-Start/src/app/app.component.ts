aea517969811f05a6166716872aa5481452bb334
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-08-21 00:33:10 +0100

    modified message service

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel, RouterEvent } from '@angular/router';
 import { AuthService } from './user/auth.service';
 import { slideInAnimation } from './app.animation';
+import { MessageService } from './messages/message.service';
 
 @Component({
   selector: 'pm-root',
@@ -25,13 +26,13 @@ export class AppComponent {
     return '';
   }
 
-  constructor(private _authService: AuthService, private _router: Router) {
+  constructor(private _authService: AuthService, private _router: Router, private _messageService: MessageService) {
     _router.events.subscribe((routerEvent: RouterEvent) => {
       this.checkRouterEvent(routerEvent);
     });
    }
 
-   checkRouterEvent(routerEvent: RouterEvent) {
+  checkRouterEvent(routerEvent: RouterEvent) {
 
       if (routerEvent instanceof NavigationStart) {
         this.loading = true;
@@ -40,10 +41,19 @@ export class AppComponent {
       if (routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationError || routerEvent instanceof NavigationCancel) {
         this.loading = false;
       }
-   }
+  }
 
   logOut(): void {
     this._authService.logout();
     console.log('Log out');
+  }
+
+  displayMessages(): void {
+    this._router.navigate([{ outlets: { popup: ['messages'] }}]);
+    this._messageService.isDisplayed = true;
+  }
+
+  hideMessages(): void {
+    this._messageService.isDisplayed = false;
   }
 }
