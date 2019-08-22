20f168238864f0b831757ca760884cf304d8630c
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-08-23 00:01:12 +0100

    created canActivate guard

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -9,12 +9,14 @@ import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
+import { AuthGuard } from '../user/auth.guard';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       { path: 'products',// component:ProductListComponent, // commented out to be replaced by a component-less route
+        canActivate: [AuthGuard],
         children: [
           // component-less route
           { path: '', 
