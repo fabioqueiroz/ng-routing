e7f319d73bb2d0e2e41a2e264facd1ba29039c0d
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2019-08-26 01:18:20 +0100

    added custom preloading

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { ProductModule } from './products/product.module';
 import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 
 @NgModule({
@@ -12,14 +13,16 @@ import { AuthGuard } from './user/auth.guard';
         RouterModule.forRoot([
             { path: 'welcome', component: WelcomeComponent},
             { path: 'products',
-              canActivate: [AuthGuard],
+              canActivate: [AuthGuard], // loads as soon as the the path is clicked
+              // canLoad: [AuthGuard], // only loads if the user logs in; blocks the PreloadAllModules
+              data: {preload: true},
               // loadChildren: () => import('./products/product.module').then(m => m.ProductModule), // not working with "module": "esnext"
-              // loadChildren: './products/product.module#ProductModule' // working
-              loadChildren: () => ProductModule
+              loadChildren: './products/product.module#ProductModule' // working
+              // loadChildren: () => ProductModule // not lazy loading
             },
             { path: '', redirectTo: 'welcome', pathMatch:'full'},
             { path: '**', component: PageNotFoundComponent}
-          ]),
+          ], {preloadingStrategy: SelectiveStrategy}), // before using a custom preloading strategy => {preloadingStrategy: PreloadAllModules}
      ],
     exports: [ RouterModule ]
 
--- APM-Start/src/app/user/auth.guard.ts
+++ APM-Start/src/app/user/auth.guard.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-
+export class AuthGuard implements CanActivate, CanLoad {
+  
   constructor(private _authService: AuthService, private _router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return this.checkLoggedIn(state.url);
+  }
+
+  // Unlike the canActivate, the canLoad method cannot access thr ActivatedRouteSnapshot or 
+  // the RouterStateSnapshot because the module defining the route is not yet loaded
+  canLoad(route: Route): boolean | Observable<boolean> | Promise<boolean> {
+    return this.checkLoggedIn(route.path);
   }
 
   checkLoggedIn(url: string) : boolean {
